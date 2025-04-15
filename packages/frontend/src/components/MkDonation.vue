35bb8de0aec3b7a0636143d7872fd39c18da53e4
Kormi Meiko <1070050130@qq.com>
2025-04-15 19:57:51 +0800

    update4.3

--- packages/frontend/src/components/MkDonation.vue
+++ packages/frontend/src/components/MkDonation.vue
@@ -6,24 +6,25 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <div class="_panel _shadow" :class="$style.root">
 	<!-- TODO: インスタンス運営者が任意のテキストとリンクを設定できるようにする -->
-	<div :class="$style.icon">
+	<!--<div :class="$style.icon">
 		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pig-money" width="40" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 			<path d="M15 11v.01"></path>
 			<path d="M5.173 8.378a3 3 0 1 1 4.656 -1.377"></path>
 			<path d="M16 4v3.803a6.019 6.019 0 0 1 2.658 3.197h1.341a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-1.342c-.336 .95 -.907 1.8 -1.658 2.473v2.027a1.5 1.5 0 0 1 -3 0v-.583a6.04 6.04 0 0 1 -1 .083h-4a6.04 6.04 0 0 1 -1 -.083v.583a1.5 1.5 0 0 1 -3 0v-2l.001 -.027a6 6 0 0 1 3.999 -10.473h2.5l4.5 -3h.001z"></path>
 		</svg>
-	</div>
+	</div>-->
 	<div :class="$style.main">
-		<div :class="$style.title">{{ i18n.ts.didYouLikeMisskey }}</div>
+		<div :class="$style.title">获取“天蓝中文桌面客户端”</div>
 		<div :class="$style.text">
-			<I18n :src="i18n.ts.pleaseDonate" tag="span">
+			通过“天蓝中文桌面客户端”，您可以获得更完整、更方便的服务。
+			<!--<I18n :src="i18n.ts.pleaseDonate" tag="span">
 				<template #host>
 					{{ instance.name ?? host }}
 				</template>
-			</I18n>
+			</I18n>-->
 			<div style="margin-top: 0.2em;">
-				<MkLink target="_blank" url="https://misskey-hub.net/docs/for-users/resources/donate/">{{ i18n.ts.learnMore }}</MkLink>
+				<MkLink target="_blank" url="https://go.skybluecn.com/link/desktop">{{ i18n.ts.learnMore }}</MkLink>
 			</div>
 		</div>
 		<div class="_buttons">
