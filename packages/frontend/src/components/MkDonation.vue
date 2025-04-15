b298f678e1b9e2ab970617cc120acb2280b1b142
Kormi Meiko <1070050130@qq.com>
2025-04-15 20:19:57 +0800

    update4.3.2

--- packages/frontend/src/components/MkDonation.vue
+++ packages/frontend/src/components/MkDonation.vue
@@ -8,10 +8,15 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<!-- TODO: インスタンス運営者が任意のテキストとリンクを設定できるようにする -->
 	<div :class="$style.icon">
 		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-desktop" width="40" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<path d="M3 5a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10z" />
+			<path d="M7 20h10" />
+			<path d="M9 16v4" /><path d="M15 16v4" />
+			
+			<!--<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 			<path d="M15 11v.01"></path>
 			<path d="M5.173 8.378a3 3 0 1 1 4.656 -1.377"></path>
-			<path d="M16 4v3.803a6.019 6.019 0 0 1 2.658 3.197h1.341a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-1.342c-.336 .95 -.907 1.8 -1.658 2.473v2.027a1.5 1.5 0 0 1 -3 0v-.583a6.04 6.04 0 0 1 -1 .083h-4a6.04 6.04 0 0 1 -1 -.083v.583a1.5 1.5 0 0 1 -3 0v-2l.001 -.027a6 6 0 0 1 3.999 -10.473h2.5l4.5 -3h.001z"></path>
+			<path d="M16 4v3.803a6.019 6.019 0 0 1 2.658 3.197h1.341a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-1.342c-.336 .95 -.907 1.8 -1.658 2.473v2.027a1.5 1.5 0 0 1 -3 0v-.583a6.04 6.04 0 0 1 -1 .083h-4a6.04 6.04 0 0 1 -1 -.083v.583a1.5 1.5 0 0 1 -3 0v-2l.001 -.027a6 6 0 0 1 3.999 -10.473h2.5l4.5 -3h.001z"></path>-->
 		</svg>
 	</div>
 	<div :class="$style.main">
